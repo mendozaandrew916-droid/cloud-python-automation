a54f0293d2ef827c06fc0f57a32e98b43b1c0a1b
Andrewww <mendozaandrew916@gmail.com>
2026-09-07 13:07:08 +0000

    Refactor monitor with custom interval loop and system status

--- sys_log_automation.py
+++ sys_log_automation.py
@@ -7,21 +7,27 @@ LOG_DIR = "system_logs"
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
-def run_monitor(iterations=3, interval=5):
-    print(f"Starting Cloud Monitor (Running {iterations} checks every {intervals}...\n)")
+# Simulated monitoring loop (Runs 4 times then stops)
+def run_monitor(iterations=4, interval=3):
 
-    for count in range (1, iteration + 1):
+    print(f"Starting Cloud Monitor (Running {iterations} checks every {interval}...\n)")
+
+    for count in range (1, iterations + 1):
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        file_poth = os.path.join(LOG_DIR, f"log_check_{count}_{time}.txt")
+        file_path = os.path.join(LOG_DIR, f"log_check_{count}_{timestamp}.txt")
 
-        with open(file_poth, "w") as f:
-            f.write(f"Check #{count}\nTimestamp: {datetime.now()}\nStatus: OPERATIONAL\n")
+        with open(file_path, "w") as f:
+            f.write(f"Check #{count}\nTimestamp: {datetime.now()}\nServer Status: ALL SYSTEMS GREEN - ANDREW MENDOZA\n")
 
-        print(f"[{count}/{iterations}] Generated: {file_poth}")
+        print(f"[{count}/{iterations}] Generated: {file_path}")
 
+        # Pause execution if not on the final iteration
         if count < iterations:
             time.sleep(interval)
 
 
-        if __name__ == "__main__":
-            run_monitor()
+if __name__ == "__main__":
+     run_monitor(iterations=4, interval=3)
+     print("Monitoring complete. All logs saved successfully!")
+
+    
